78b8d21a1e6a8e434536e2c995755006d894bbd2
sclamac <sclamac@connect.ust.hk>
2016-11-15 11:58:23 +0800

    Add files via upload

--- app/assets/js/signup.js
+++ app/assets/js/signup.js
@@ -23,7 +23,8 @@ $scope.SignUp = function() {
                         name: username,
                         email: email ,
                         createdate: new Date().toString()
-            })
+            })            $window.alert("Register Success! ");
+            window.location.href= "index.html";
 			      
 			
 
